b6301f07ae3c8380374be9df3595c870a5070fdc
Dhruv964 <91016974+Dhruv964@users.noreply.github.com>
2024-03-10 11:38:45 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,11 +35,6 @@ export default function page() {
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">
-            Hi, Welcome back Leadenhall
-          </h2>
-        </div>
 
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
           <Card className="col-span-2">
